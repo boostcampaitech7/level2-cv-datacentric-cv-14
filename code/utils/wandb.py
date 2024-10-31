5b55ac61ec6138756c8a73bc1a3809718a35d662
oweixx <lowzxx@naver.com>
2024-10-31 02:54:29 +0000

    [#18] Feature : Implement wandb setup

---
+++ code/utils/wandb.py
@@ -0,0 +1,16 @@
+import wandb
+
+def set_wandb(name, detail, configs) :
+    wandb.init(
+        name=detail,
+        project=name,
+        config={
+                'seed': configs['seed'],
+                'split': configs['split'],
+                'image_size': configs['image_size'],
+                'input_size': configs['input_size'],
+                'batch_size': configs['batch_size'],
+                'learning_rate': configs['learning_rate'],
+                'epoch': configs['max_epoch']
+            }
+    )
