1b9577558d526c6b14a5cab1b54a5074962c9157
kimgeonsu <thdudlee0617@gmail.com>
2024-11-04 01:41:31 +0900

    [#22] Feature : 언어별 영수증 합성 이미지 100개씩 생성

--- code/synth_data/trdg_maker.py
+++ code/synth_data/trdg_maker.py
@@ -1,38 +1,43 @@
-from trdg.generators import GeneratorFromRandom
+from trdg.generators import GeneratorFromStrings
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 from PIL import Image
-from synth_utils import draw_bbox, make_document, Document
+from synth_utils import make_document, Document
 import os
 import time
 import json
 
 def get_words(count=128):
     words = []
-    for _ in range(count):
+    word_dict = []
+    with open('./lang_example/japanese_receipt_dict.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            words_arr = line.strip().split()
+            word_dict.extend(words_arr)
+
+    np.random.shuffle(word_dict)
+    
+    for idx in range(count):
         margins = [np.random.randint(3, 8) for _ in range(4)]  # 상하좌우 각각 다른 마진 생성
         character_spacing = np.random.randint(1, 30)
-        size = np.random.randint(64, 200)  # 이미지 크기를 32~200 사이에서 랜덤하게 설정
-        generator = GeneratorFromRandom(
-            language="cn", 
-            count=2,
+        size = np.random.randint(64, 200)  # 이미지 크기를 64~200 사이에서 랜덤하게 설정
+        generator = GeneratorFromStrings(
+            strings=[word_dict[idx]],
+            language="ja", 
+            count=1,
             background_type=0,
             fit=True,
-            image_dir='./data',
             margins=tuple(margins),  # 4개의 다른 마진 값 전달
             size=size,  # 랜덤한 이미지 크기 적용
             character_spacing=character_spacing,  # 랜덤한 글자 간격 적용
             blur=3,
             random_blur=True,
-            allow_variable=True,
-            length=1,
             distorsion_type=1,
         )
         for _, (patch, text) in enumerate(generator):
             words.append({"patch": patch, "text": text, "size": patch.size, "margins": margins})
     return words
-
 
 def vm_function(bbox: list[float], M: np.ndarray) -> np.ndarray:
     v = np.array(bbox).reshape(-1, 2).T
@@ -79,21 +84,21 @@ def perturb_document_inplace(document: Document, pad=0, color=None) -> Document:
 
 
 # 디렉토리 생성
-os.makedirs("./synth_data/img/train", exist_ok=True)
-os.makedirs("./synth_data/train/ufo", exist_ok=True)
+os.makedirs("./synth_receipt_data/japanese/img/train", exist_ok=True)
+os.makedirs("./synth_receipt_data/japanese/train/ufo", exist_ok=True)
 
 # UFO format json 데이터 초기화
 ufo_data = {"images": {}}
 
 # 이미지 개수가 100개가 될 때까지 생성
-while len(os.listdir("./synth_data/img/train")) < 100:
+while len(os.listdir("./synth_receipt_data/japanese/img/train")) < 100:
     # 문서 생성 및 변형
     doc = make_document(get_words(30))
     perturb_document_inplace(doc)
     
     # 이미지 파일명 생성
-    img_filename = f"synthetic_image_{time.strftime('%Y%m%d_%H%M%S')}_{len(os.listdir('./synth_data/img/train')):03d}.jpg"
-    img_path = f"./synth_data/img/train/{img_filename}"
+    img_filename = f"synthetic_image_{time.strftime('%Y%m%d_%H%M%S')}_{len(os.listdir('./synth_receipt_data/japanese/img/train')):03d}.jpg"
+    img_path = f"./synth_receipt_data/japanese/img/train/{img_filename}"
     
     # 이미지 저장
     doc["image"].save(img_path)
@@ -137,6 +142,6 @@ while len(os.listdir("./synth_data/img/train")) < 100:
     time.sleep(1)
 
 # 모든 이미지에 대한 JSON 파일 저장
-json_path = "./synth_data/train/ufo/train.json"
+json_path = "./synth_receipt_data/japanese/train/ufo/train.json"
 with open(json_path, 'w', encoding='utf-8') as f:
     json.dump(ufo_data, f, ensure_ascii=False, indent=4)
