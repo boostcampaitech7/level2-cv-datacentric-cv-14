d57e0487aa63888a87a24292a60befbdeace6e43
lkl4502 <lkl4502@naver.com>
2024-11-07 18:56:36 +0900

    [#42] Refactor : Update Model checkpoint Name

--- code/train.py
+++ code/train.py
@@ -175,7 +175,7 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
                         best_f1_score = val_log_dict['Valid F1 Score']
                         best_f1_score_epoch = epoch
                         
-                        save_path = osp.join(model_dir, f"best_f1_score_{best_f1_score:.4f}_{best_f1_score_epoch}epoch_.pth")
+                        save_path = osp.join(model_dir, f"best_f1_score_{best_f1_score:.4f}_{best_f1_score_epoch}epoch.pth")
                         counter = 0
                     else:
                         counter += 1
